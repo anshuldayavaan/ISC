768a8f660a0f12f341e7d89d5cbfa742481ded44
Anshul Dayavaan <anshuldayavaan@gmail.com>
2019-06-28 13:23:05 +0530

    Create prog_3.java

---
+++ prog_3.java
@@ -0,0 +1,12 @@
+public class prog_3
+{
+   public static void main ()
+   {
+   int a=1, b=2, c=3, big ;
+
+ 
+    big = a > b ? (a > c ? a : c) : (b > c ? b : c) ;
+ 
+    System.out.println("The biggest number is :"+big) ;
+}
+}
